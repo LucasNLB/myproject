f3eda74d74239c243b19877a17fe0b428d49509d
LucasNLB <buit.bgvmm@gmail.com>
2021-10-24 14:21:17 +0700

    Update DB.php

--- mvc/core/DB.php
+++ mvc/core/DB.php
@@ -3,15 +3,10 @@
 class DB{
 
     public $con;
-    $host = "ec2-54-224-194-214.compute-1.amazonaws.com";
-    $database = "dsp51t0avtc5r";
-    $user = "sqqzeidvgegbly";
-    $port = "5432";
-    $password = "0cf33ebbadc2c4b7f7920497fd864e3d1e89c1fde423f9b1ed7bab8c8645f5d8";
+    
 
-    $connection_string = "host=".$host." user=".$user." dbname=".$database." port=".$port." password=".$password." sslmode=require";
-    $connection = pg_connect($connection_string);
-    protected $$host = "ec2-54-224-194-214.compute-1.amazonaws.com";
+    
+    protected $host = "ec2-54-224-194-214.compute-1.amazonaws.com";
     protected $database = "dsp51t0avtc5r";
     protected $user = "sqqzeidvgegbly";
     protected $port = "5432";
@@ -20,7 +15,10 @@ class DB{
 
 
     function __construct(){
-        $this->con = pg_connect("host=".$this->servername."port=".$this->port."dbname=".$this->dbname."user=".$this->username."password=".$this->password."sllmode=".$this->ssl);
+
+        $connection_string = "host=".$this->host." user=".$this->user." dbname=".$this->database." port=".$this->port." password=".$this->password." sslmode=require";
+        $this->con = pg_connect($connection_string);
+        
         
     }
     
