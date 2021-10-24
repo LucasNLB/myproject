8e4736cc0c22598df29cbafa530e432414e55195
LucasNLB <buit.bgvmm@gmail.com>
2021-10-24 14:12:04 +0700

    Update Home.php

--- mvc/controllers/Home.php
+++ mvc/controllers/Home.php
@@ -8,21 +8,20 @@ class Home extends Controller{
     function SayHi(){
 
          
+        // $host = "ec2-54-224-194-214.compute-1.amazonaws.com";
+        // $database = "dsp51t0avtc5r";
+        // $user = "sqqzeidvgegbly";
+        // $port = "5432";
+        // $password = "0cf33ebbadc2c4b7f7920497fd864e3d1e89c1fde423f9b1ed7bab8c8645f5d8";
+
+        // $connection_string = "host=".$host." user=".$user." dbname=".$database." port=".$port." password=".$password." sslmode=require";
+        // $connection = pg_connect($connection_string);
+
+
         
-         $servername = "ec2-54-224-194-214.compute-1.amazonaws.com";
-         $username = "sqqzeidvgegbly";
-         $port = "5432";
-         $password = "0cf33ebbadc2c4b7f7920497fd864e3d1e89c1fde423f9b1ed7bab8c8645f5d8";
-         $dbname = "dsp51t0avtc5r";
-         $ssl = "require";
-         $con = pg_connect("host=".$servername."port=".$port."dbname=".$dbname."user=".$username."password=".$password."sllmode=".$ssl);
-        
-        //call model
-        $product_query = 'INSERT INTO public."PRODUCTTest"(id,name,price) VALUES (\''."1".'\',\''."Bình".'\',\''."1000".'\')';
-        pg_query($con, $product_query);
 
         //call views
-        //$this->view("test");
+        $this->view("test");
     }
 
     function Show($a, $b){        
