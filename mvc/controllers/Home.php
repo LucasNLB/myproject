feb004f6d9f3ceef97294c92ff80dbde17b5df7b
LucasNLB <buit.bgvmm@gmail.com>
2021-10-24 14:21:11 +0700

    Update Home.php

--- mvc/controllers/Home.php
+++ mvc/controllers/Home.php
@@ -6,25 +6,12 @@ class Home extends Controller{
 
     // Must have SayHi()
     function SayHi(){
+        $this->model("SinhVienModel");
 
          
-        $host = "ec2-54-224-194-214.compute-1.amazonaws.com";
-        $database = "dsp51t0avtc5r";
-        $user = "sqqzeidvgegbly";
-        $port = "5432";
-        $password = "0cf33ebbadc2c4b7f7920497fd864e3d1e89c1fde423f9b1ed7bab8c8645f5d8";
-
-        $connection_string = "host=".$host." user=".$user." dbname=".$database." port=".$port." password=".$password." sslmode=require";
-        $connection = pg_connect($connection_string);
-        if($connection === false)
-            die("ERROR : could not connet to datebase");
-        else
-            echo "SUCESS: connetion to HERUKU pastages has been establist";
-
-
         
-
-        //call views
+        
+   //call views
         // $this->view("test");
     }
 
