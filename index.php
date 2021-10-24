96f1378c3872638faf84e1bd7a8fedaf14066e92
LucasNLB <buit.bgvmm@gmail.com>
2021-10-24 12:12:55 +0700

    .

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-session_start();
-require_once "../mvc/Bridge.php";
-$myApp = new App();
+// session_start();
+// require_once "./mvc/Bridge.php";
+// $myApp = new App();
+echo "Bình đẹp trai";
 ?>
