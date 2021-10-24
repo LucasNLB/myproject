922c259562fa32f4e8a47ae46f3bbb6723519126
LucasNLB <buit.bgvmm@gmail.com>
2021-10-24 12:14:56 +0700

    .

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-// session_start();
-// require_once "./mvc/Bridge.php";
-// $myApp = new App();
-echo "Bình đẹp trai";
+    session_start();
+    require_once "./mvc/Bridge.php";
+    $myApp = new App();
 ?>
--- mvc/Bridge.php
+++ mvc/Bridge.php
@@ -1,10 +1,12 @@
 <?php
-// Process URL from browser
-require_once "./mvc/core/App.php";
+// // Process URL from browser
+// require_once "./mvc/core/App.php";
 
-// How controllers call Views & Models
-require_once "./mvc/core/Controller.php";
+// // How controllers call Views & Models
+// require_once "./mvc/core/Controller.php";
 
-// Connect Database
-require_once "./mvc/core/DB.php";
+// // Connect Database
+// require_once "./mvc/core/DB.php";
+
+echo "Bình Đẹp trai lắm";
 ?>
