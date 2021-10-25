20fda88540bb4ad24d301b05eea609d3e52cf2c9
LucasNLB <buit.bgvmm@gmail.com>
2021-10-25 18:38:57 +0700

    Update HomepageModel.php

--- mvc/models/HomepageModel.php
+++ mvc/models/HomepageModel.php
@@ -3,7 +3,7 @@ class HomepageModel extends DB{
 
     public function homeimg(){
 
-        $img = "SELECT * FROM public.test";
+        $img = "SELECT * FROM public.ImgHomePage";
         return pg_query($this->con, $img);
 
     }
