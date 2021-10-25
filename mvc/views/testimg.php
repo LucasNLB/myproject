f0f66f1c1957b3d85f110215967d26ac2812dfc2
LucasNLB <buit.bgvmm@gmail.com>
2021-10-25 16:10:12 +0700

    Update testimg.php

--- mvc/views/testimg.php
+++ mvc/views/testimg.php
@@ -6,8 +6,7 @@ $user = "sqqzeidvgegbly";
 $port = "5432";
 $password = "0cf33ebbadc2c4b7f7920497fd864e3d1e89c1fde423f9b1ed7bab8c8645f5d8";
 
-$connection_string = "host=". $host." user=". $user." dbname=". $database." port=". $port." password=". $password." sslmode=require";
-
+$connection_string = "host=".$host." user=".$user." dbname=".$database." port=".$port." password=".$password." sslmode=require";
 $connection = pg_connect($connection_string);
 if($connection === false)
         echo $connection_string;
