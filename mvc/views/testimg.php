5a51b9c7deb90aa158b7d848f078fb94014da465
LucasNLB <buit.bgvmm@gmail.com>
2021-10-25 16:46:09 +0700

    Update testimg.php

--- mvc/views/testimg.php
+++ mvc/views/testimg.php
@@ -8,11 +8,11 @@ $password = "0cf33ebbadc2c4b7f7920497fd864e3d1e89c1fde423f9b1ed7bab8c8645f5d8";
 
 $connection_string = "host=".$host." user=".$user." dbname=".$database." port=".$port." password=".$password." sslmode=require";
 $connection = pg_connect($connection_string);
+
+
 if($connection === false)
-        echo $connection_string;
-    else
-        echo "SUCESS: connetion to HERUKU pastages has been establist";
-
-
+    die("ERROR : could not connet to datebase");
+else
+    echo "SUCESS: connetion to HERUKU pastages has been establist";
 
 ?>
