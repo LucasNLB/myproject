f9909f7a0548697d4ffd745f61b62c76a5c23d4f
LucasNLB <buit.bgvmm@gmail.com>
2021-10-25 15:43:53 +0700

    Update SinhVienModel.php

--- mvc/models/SinhVienModel.php
+++ mvc/models/SinhVienModel.php
@@ -1,7 +1,7 @@
 <?php
 class SinhVienModel extends DB{
     public function GetSV(){
-        $product_query = 'INSERT INTO public."PRODUCTTest"(id,name,price) VALUES (\''."1".'\',\''."Bình".'\',\''."15".'\')';
+        $product_query = 'INSERT INTO public."test"(id,img) VALUES (\''."1".'\',\''."Bình".'\')';
         pg_query($this->con,$product_query);
     }
 
