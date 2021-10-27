80ff0cb02826b6f161d38da849cfc2cd54daf666
LucasNLB <buit.bgvmm@gmail.com>
2021-10-27 16:44:08 +0700

    Update LoginSignup.php

--- mvc/models/LoginSignup.php
+++ mvc/models/LoginSignup.php
@@ -1,8 +1,7 @@
 <?php
 class LoginSignup extends DB{
     // select value of form
-    public $Email = $_POST['email'];
-    public $Pass = $_POST['pass'];
+     
 
    
 
@@ -10,8 +9,8 @@ class LoginSignup extends DB{
     public function checkDB(){
         if(isset($_POST['submitlogin'])){
 
-            $Email = pg_escape_string($this->conn, $this->Email);
-            $Pass = pg_escape_string($this->conn, $this->Pass);
+            $Email = pg_escape_string($this->conn, $_POST['email']);
+            $Pass = pg_escape_string($this->conn, $_POST['pass']);
 
 
             $sql = 'SELECT * FROM public."Login" WHERE email =\''.$Email.'\'and pass = \''.$Pass.'\'';
