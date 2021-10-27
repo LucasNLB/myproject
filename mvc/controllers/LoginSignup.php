cf2a42c2490464f8ed8c71b46d884eb9cb0fc7ad
LucasNLB <buit.bgvmm@gmail.com>
2021-10-27 15:23:12 +0700

    Update LoginSignup.php

--- mvc/controllers/LoginSignup.php
+++ mvc/controllers/LoginSignup.php
@@ -23,12 +23,14 @@ class LoginSignup extends Controller{
         $sql =   $login->checkDB();
         $mum_row = pg_num_rows($sql);
         if($mum_row != 0 ){
-          $this->view("testimg");
+          
         }
         else if($mum_row == 0){
           echo("Tài Khoản hoăc Mật Khẩu sai !");
         }
       }
+
+    $this->view("testimg");
     }
 
     function Show($a, $b){        
