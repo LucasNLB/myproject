c45b336c2c879f5c58ad4991015a14a75c3f9dac
LucasNLB <buit.bgvmm@gmail.com>
2021-11-01 16:48:18 +0700

    Update ProductDetail.php

--- mvc/views/ProductDetail.php
+++ mvc/views/ProductDetail.php
@@ -19,17 +19,19 @@
 
 
 <div class="container">
+if(pg_num_rows($data["img"]) > 0){
+            while($row = pg_fetch_assoc($data["img"])){ ?>
     <div class="row">
         <div class="col-xs-8 col-sm-8" ><img  src="../../img/<?php echo $row['nameimg']; ?>" alt="" width="100%" height="450vh" ></div>
         <div class="col-xs-4 col-sm-4">
             <div class="productName">ádasdasd</div>
             <div class="priceProduct">15000000</div>
             <div class="box-oder">
-                <button class="btnOder">Thêm vào giỏ hảng</button> <br>
+                <button class="btnOder">Thêm vào giỏ hảng</button><br>
                 <button class="btnOder">Mua ngay</button>
             </div>
         </div>
-    </div>
+    </div> }} ?> 
 </div>
 
 <div class="container">
