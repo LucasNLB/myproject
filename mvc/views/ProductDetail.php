29a52a581a7ec37ce6e304b74ab0ff860887fd01
LucasNLB <buit.bgvmm@gmail.com>
2021-11-01 11:49:02 +0700

    .

--- mvc/views/ProductDetail.php
+++ mvc/views/ProductDetail.php
@@ -11,7 +11,7 @@
     <script src="jquery.min.js"></script>
     <script src="owlcarousel/owl.carousel.min.js"></script>
     <link rel="stylesheet" href="../orther/bootstrap-5.1.3-dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/ProductDetail.css">
+    <link rel="stylesheet" href="../../css/ProductDetail.css">
 </head>
 
 <body>
