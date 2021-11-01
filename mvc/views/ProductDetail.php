5bc3256d3fe5bc0608651141a842cd48c204abac
LucasNLB <buit.bgvmm@gmail.com>
2021-11-01 11:43:03 +0700

    Update ProductDetail.php

--- mvc/views/ProductDetail.php
+++ mvc/views/ProductDetail.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../css/Pagemain.css">
+    <link rel="stylesheet" href="../../css/Pagemain.css">
     <link rel="stylesheet" href="owlcarousel/owl.carousel.min.css">
     <link rel="stylesheet" href="owlcarousel/owl.theme.default.min.css">
     <script src="jquery.min.js"></script>
