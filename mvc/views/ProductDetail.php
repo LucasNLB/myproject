caeaac9e71958459e30e2198e986cc3582647bb8
LucasNLB <buit.bgvmm@gmail.com>
2021-11-02 10:38:27 +0700

    Update ProductDetail.php

--- mvc/views/ProductDetail.php
+++ mvc/views/ProductDetail.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="owlcarousel/owl.theme.default.min.css">
     <script src="jquery.min.js"></script>
     <script src="owlcarousel/owl.carousel.min.js"></script>
-    <link rel="stylesheet" href="../orther/bootstrap-5.1.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../other/bootstrap-5.1.3-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="../../css/ProductDetail.css">
 </head>
 
